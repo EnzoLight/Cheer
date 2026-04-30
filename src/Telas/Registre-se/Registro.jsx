9b8bd4fd09afb1ac3b0d9db7d6540582a2368089
EnzoLight <leducof@gmail.com>
2026-04-30 23:40:55 +0000

    Mais parciais das telas

--- src/Telas/Registre-se/Registro.jsx
+++ src/Telas/Registre-se/Registro.jsx
@@ -99,19 +99,19 @@ function Registro() {
                 }}>
                 <h1 style={{ display: 'flex', flexDirection: 'column', marginBottom: '20px', textAlign: 'center', fontSize: '1.5rem', fontFamily: 'Mogra' }}>PREENCHA OS CAMPOS ABAIXO</h1>
                 <div style={{ backgroundColor: '#FF8C00', paddingLeft: '50px', paddingRight: '50px', padding: '20px', borderRadius: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                  <label style={{ textAlign: 'center', width: '80%' }}>Nome Completo:</label>
+                  <label style={{ textAlign: 'center', width: '80%', fontFamily:'Source-Sans-3', fontWeight:'bold' }}>Nome Completo:</label>
                   <input style={{ width: '300px', height: '30px', marginBottom: '10px' }} type="text" placeholder="Digite seu nome completo" />
 
-                  <label style={{ textAlign: 'center', width: '80%' }}>Email:</label>
+                  <label style={{ textAlign: 'center', width: '80%', fontFamily:'Source-Sans-3', fontWeight:'bold'}}>Email:</label>
                   <input style={{ width: '300px', height: '30px', marginBottom: '10px' }} type="email" placeholder="Digite seu email" />
 
-                  <label style={{ textAlign: 'center', width: '80%' }}>Telefone:</label>
+                  <label style={{ textAlign: 'center', width: '80%', fontFamily:'Source-Sans-3', fontWeight:'bold' }}>Telefone:</label>
                   <input style={{ width: '300px', height: '30px', marginBottom: '10px' }} type="tel" placeholder="Digite seu telefone" />
 
-                  <label style={{ textAlign: 'center', width: '80%' }}>CPF:</label>
+                  <label style={{ textAlign: 'center', width: '80%', fontFamily:'Source-Sans-3', fontWeight:'bold' }}>CPF:</label>
                   <input style={{ width: '300px', height: '30px', marginBottom: '10px' }} type="text" placeholder="Digite seu CPF" />
 
-                  <label style={{ textAlign: 'center', width: '80%' }}>Senha:</label>
+                  <label style={{ textAlign: 'center', width: '80%', fontFamily:'Source-Sans-3', fontWeight:'bold' }}>Senha:</label>
                   <input
                     style={{ width: '300px', height: '30px', marginBottom: '10px' }}
                     type="password"
@@ -120,7 +120,7 @@ function Registro() {
                     onChange={(e) => setPassword(e.target.value)}
                   />
 
-                  <label style={{ textAlign: 'center', width: '80%' }}>Confirmar Senha:</label>
+                  <label style={{ textAlign: 'center', width: '80%', fontFamily:'Source-Sans-3', fontWeight:'bold' }}>Confirmar Senha:</label>
                   <input style={{ width: '300px', height: '30px', marginBottom: '7px' }} type="password" placeholder="Confirme sua senha" />
                 </div>
 
@@ -137,47 +137,52 @@ function Registro() {
                     display: 'flex', 
                     flexDirection: 'column', 
                     justifyContent: 'center', 
-                    alignItems: 'center' }}>
+                    alignItems: 'flex-start' }}>
 
-                  <input
-                    type="checkbox"
-                    checked={validations.hasUpper}
-                    readOnly
-                    className="form-check-input me-2"
-                  />
-                  <span style={{ color: validations.hasUpper ? 'green' : 'red' }}>
-                    Contêm uma letra maiúscula
-                  </span>
-
-                  <input
-                    type="checkbox"
-                    checked={validations.hasNumber}
-                    readOnly
-                    className="form-check-input me-2"
-                  />
-                  <span style={{ color: validations.hasNumber ? 'green' : 'red' }}>
-                    Pelo menos um número
-                  </span>
-
-                  <input
-                    type="checkbox"
-                    checked={validations.minLength}
-                    readOnly
-                    className="form-check-input me-2"
-                  />
-                  <span style={{ color: validations.minLength ? 'green' : 'red' }}>
-                    Mínimo de 8 caracteres
-                  </span>
-
-                  <input
-                    type="checkbox"
-                    checked={validations.hasSpecial}
-                    readOnly
-                    className="form-check-input me-2"
-                  />
-                  <span style={{ color: validations.hasSpecial ? 'green' : 'red' }}>
-                    Contem um caractere especial (!@#$%^&*)
-                  </span>
+                    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                      <input
+                        type="checkbox"
+                        checked={validations.hasUpper}
+                        readOnly
+                        className="form-check-input me-2"
+                      />
+                      <span style={{  color: validations.hasUpper ? 'green' : 'red', fontFamily:'Source-Sans-3' }}>
+                        Contêm uma letra maiúscula
+                      </span>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                      <input
+                        type="checkbox"
+                        checked={validations.hasNumber}
+                        readOnly
+                        className="form-check-input me-2"
+                      />
+                      <span style={{ color: validations.hasNumber ? 'green' : 'red', fontFamily:'Source-Sans-3' }}>
+                        Pelo menos um número
+                      </span>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                      <input
+                        type="checkbox"
+                        checked={validations.minLength}
+                        readOnly
+                        className="form-check-input me-2"
+                      />
+                      <span style={{ color: validations.minLength ? 'green' : 'red', fontFamily:'Source-Sans-3' }}>
+                        Mínimo de 8 caracteres
+                      </span>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                      <input
+                        type="checkbox"
+                        checked={validations.hasSpecial}
+                        readOnly
+                        className="form-check-input me-2"
+                      />
+                      <span style={{ color: validations.hasSpecial ? 'green' : 'red', fontFamily:'Source-Sans-3' }}>
+                        Contem um caractere especial (!@#$%^&*)
+                      </span>
+                    </div>
                 </div>
                 <button id='btn_registro_user' style={{ marginTop: '10px', width: '200px', height: '50px', fontSize: '1.25rem' }} class="btn btn-primary">Criar Conta</button>
               </div>
