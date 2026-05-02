0762481844d82808fe6c02bb0ea4404036cfba13
Matheus Holanda <matheusnh6@gmail.com>
2026-05-02 18:50:22 -0300

    Tela de perfil

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './Telas/Home/App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Registro from './Telas/Registre-se/Registro'
 import Evento from './Telas/Eventos/Evento'
-
+import Perfil from './Telas/Perfil/Perfil'
 
 // Note que aqui usamos apenas 'createRoot' (sem o ReactDOM antes)
 createRoot(document.getElementById('root')).render(
@@ -17,6 +17,7 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<App />} />
         <Route path="/registro" element={<Registro />} />
         <Route path="/eventos" element={<Evento />} />
+        <Route path="/perfil" element={<Perfil />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
